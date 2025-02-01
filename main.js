7c0fd3f930c21919c988239e6b7160dff4c09d2a
SerbCSharp <serbcsharp@mail.ru>
2025-02-01 16:07:07 +0300

    Init

---
+++ main.js
@@ -0,0 +1,89 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+//import GUI from 'lil-gui';
+import { FontLoader } from  'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
+//const gui = new GUI();
+
+const scene = new THREE.Scene();
+
+const textureLoader = new  THREE.TextureLoader();
+const matcapTexture = textureLoader.load('/static/textures/matcaps/8.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
+
+const fontLoader = new FontLoader();
+fontLoader.load('/static/fonts/helvetiker_regular.typeface.json', (font) => {
+    const textCeometry = new TextGeometry('Air Battle', {
+		font: font,
+		size: 0.5,
+		depth: 0.2,
+		curveSegments: 5,
+		bevelEnabled: true,
+		bevelThickness: 0.03,
+		bevelSize: 0.02,
+		bevelOffset: 0,
+		bevelSegments: 4
+	})
+    textCeometry.center();
+    const material = new THREE.MeshMatcapMaterial();
+    material.matcap = matcapTexture;
+    const text = new THREE.Mesh(textCeometry, material);
+    scene.add(text);
+
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+    for(let i = 0; i < 200; i++)
+    {
+        const donut = new THREE.Mesh(donutGeometry, material);
+        donut.position.x = (Math.random() - 0.5) * 10;
+        donut.position.y = (Math.random() - 0.5) * 10;
+        donut.position.z = (Math.random() - 0.5) * 10;
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+        const scale = Math.random();
+        donut.scale.set(scale, scale, scale);
+        scene.add(donut);
+    }
+});
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.append( renderer.domElement );
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
+window.addEventListener('dblclick', () => {
+    if(!document.fullscreenElement) {
+        renderer.domElement.requestFullscreen()
+    }
+    else {
+        document.exitFullscreen();
+    }
+})
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    orbitControls.update();
+    renderer.render( scene, camera );
+    window.requestAnimationFrame(tick)
+}
+
+tick();
