1ceb050e31d572605268f50ca8b8a69901d49f38
SerbCSharp <serbcsharp@mail.ru>
2025-02-02 13:48:31 +0300

    Baked shadow

--- main.js
+++ main.js
@@ -6,35 +6,64 @@ const gui = new GUI();
 
 const scene = new THREE.Scene();
 
+const textureLoader = new THREE.TextureLoader();
+const bakedShadow = textureLoader.load('/bakedShadow.jpg')
+
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
 gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
 directionalLight.position.set(2, 2, - 1)
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.width = 1024;
 directionalLight.shadow.mapSize.height = 1024;
 directionalLight.shadow.camera.near = 1;
 directionalLight.shadow.camera.far = 6;
-
 directionalLight.shadow.camera.top = 2;
 directionalLight.shadow.camera.right = 2;
 directionalLight.shadow.camera.bottom = - 2;
 directionalLight.shadow.camera.left = - 2;
-
+//directionalLight.shadow.radius = 10;
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
 directionalLightCameraHelper.visible = false;
 scene.add(directionalLightCameraHelper);
 gui.add(directionalLightCameraHelper, 'visible');
-
 gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
+
+const spotLight = new THREE.SpotLight(0xffffff, 0.9, 10, Math.PI * 0.3);
+spotLight.castShadow = true;
+spotLight.position.set(0, 2, 2);
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.fov = 30;
+spotLight.shadow.camera.near = 1;
+spotLight.shadow.camera.far = 6;
+scene.add(spotLight);
+scene.add(spotLight.target);
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+spotLightCameraHelper.visible = false;
+scene.add(spotLightCameraHelper);
+gui.add(spotLightCameraHelper, 'visible');
+
+const pointLight = new THREE.PointLight(0xffffff, 1.3)
+pointLight.castShadow = true;
+pointLight.position.set(- 1, 1, 0);
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+pointLight.shadow.camera.near = 0.1;
+pointLight.shadow.camera.far = 5;
+scene.add(pointLight);
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+pointLightCameraHelper.visible = false;
+scene.add(pointLightCameraHelper);
+gui.add(pointLightCameraHelper, 'visible');
 
 const material = new THREE.MeshStandardMaterial()
 material.roughness = 0.7
@@ -49,7 +78,7 @@ sphere.castShadow = true;
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
-    material
+    new THREE.MeshBasicMaterial({ map: bakedShadow })
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
@@ -66,7 +95,8 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
+renderer.shadowMap.enabled = false;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.append( renderer.domElement );
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
