654dfb163807a93ff0eb263637ff48529c753a9c
SerbCSharp <serbcsharp@mail.ru>
2025-02-13 18:37:08 +0300

    Raycaster

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -16,57 +15,37 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath('/draco/')
-const gltfLoader = new GLTFLoader();
-gltfLoader.setDRACOLoader(dracoLoader)
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = - 2
 
-let mixer = null
-
-gltfLoader.load('/models/Fox/glTF/Fox.gltf',
-    (gltf) =>
-    {
-        mixer = new THREE.AnimationMixer(gltf.scene)
-
-        const action = mixer.clipAction(gltf.animations[2])
-        action.play()
-
-        gltf.scene.scale.set(0.025, 0.025, 0.025)
-        scene.add(gltf.scene)
-    }
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
-floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
+object3.position.x = 2
 
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
-scene.add(ambientLight)
+scene.add(object1, object2, object3)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+const raycaster = new THREE.Raycaster()
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection)
+// const intersect = raycaster.intersectObject(object2)
+// const intersects = raycaster.intersectObjects(object1, object2, object3)
+
+
 
 /**
  * Sizes
@@ -91,17 +70,30 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+const mouse = new THREE.Vector2();
+window.addEventListener('mousemove', (event) =>
+{
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+})
+
+window.addEventListener('click', (event) =>
+    {
+        if(currentIntersect)
+        {
+            
+        }
+    })
 /**
  * Camera
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 2, 2)
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
 controls.enableDamping = true
 
 /**
@@ -110,27 +102,94 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const gltfLoader = new GLTFLoader();
+let model = null;
+gltfLoader.load('/models/Duck/glTF-Binary/Duck.glb', (gltf) => 
+{
+    model = gltf.scene
+    model.position.y = - 1.2
+    scene.add(model)
+})
+
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.3)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 0.7);
+scene.add(directionalLight);
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-let previousTime = 0
+let currentIntersect = null
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
 
-    if(mixer !== null)
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+    raycaster.setFromCamera(mouse, camera);
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+    for(const object of objectsToTest)
     {
-        mixer.update(deltaTime)
+        object.material.color.set('#ff0000')
     }
+    for(const intersect of intersects)
+    {
+        intersect.object.material.color.set('#0000ff')
+    }
+
+    if(intersects.length)
+    {
+        if(currentIntersect === null)
+        {
+
+        }
+        currentIntersect = intersects[0]
+    }
+    else
+    {
+        if(currentIntersect)
+        {
+
+        }
+        currentIntersect = null
+    }
+
+    if(model)
+    {
+        const modelIntersects = raycaster.intersectObject(model)
+        if(modelIntersects.length)
+            {
+                model.scale.set(1.2, 1.2, 1.2)
+            }
+        else
+            {
+                model.scale.set(1, 1, 1)
+            }
+    }
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(1, 0, 0)
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection)
+// const objectsToTest = [object1, object2, object3]
+// const intersects = raycaster.intersectObjects(objectsToTest)
+// for(const object of objectsToTest)
+// {
+//     object.material.color.set('#ff0000')
+// }
+// for(const intersect of intersects)
+// {
+//     intersect.object.material.color.set('#0000ff')
+// }
 
     // Update controls
     controls.update()
