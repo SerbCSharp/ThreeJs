83f959cf239d18597c3b6c3d584106361604d163
SerbCSharp <serbcsharp@mail.ru>
2025-02-23 07:53:06 +0300

    Proba

---
+++ Experience/World/World.js
@@ -0,0 +1,22 @@
+import * as THREE from 'three'
+import Environment  from './Environment.js'
+
+export default class World
+{
+    constructor(experience)
+    {
+        this.experience = experience
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        const testMesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshStandardMaterial()
+        )
+        this.scene.add(testMesh)
+        this.resources.on('ready', () =>
+        {
+            this.environment = new Environment(this.experience)
+        })
+    }
+}
