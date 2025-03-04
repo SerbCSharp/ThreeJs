4f5e188a8a8471fe0d361a8456138ce4b0968d2c
SerbCSharp <serbcsharp@mail.ru>
2025-03-04 18:00:51 +0300

    Fireworks Shaders

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import holographicVertexShader from './shaders/holographic/vertex.glsl'
-import holographicFragmentShader from './shaders/holographic/fragment.glsl'
+import gsap from 'gsap'
+import { Sky } from 'three/addons/objects/Sky.js'
+import fireworkVertexShader from './shaders/firework/vertex.glsl'
+import fireworkFragmentShader from './shaders/firework/fragment.glsl'
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI()
+const gui = new GUI({ width: 340 })
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -18,21 +19,25 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Loaders
-const gltfLoader = new GLTFLoader()
+const textureLoader = new THREE.TextureLoader()
 
 /**
  * Sizes
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2)
 }
+sizes.resolution = new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
 window.addEventListener('resize', () =>
 {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+    sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -40,7 +45,7 @@ window.addEventListener('resize', () =>
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setPixelRatio(sizes.pixelRatio)
 })
 
 /**
@@ -48,7 +53,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(7, 7, 7)
+camera.position.set(1.5, 0, 6)
 scene.add(camera)
 
 // Controls
@@ -58,108 +63,155 @@ controls.enableDamping = true
 /**
  * Renderer
  */
-const rendererParameters = {}
-rendererParameters.clearColor = '#1d1f2a'
-
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
 })
-renderer.setClearColor(rendererParameters.clearColor)
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(sizes.pixelRatio)
 
-gui
-    .addColor(rendererParameters, 'clearColor')
-    .onChange(() =>
-    {
-        renderer.setClearColor(rendererParameters.clearColor)
-    })
+const textures = [
+    textureLoader.load('/particles/1.png'),
+    textureLoader.load('/particles/2.png'),
+    textureLoader.load('/particles/3.png'),
+    textureLoader.load('/particles/4.png'),
+    textureLoader.load('/particles/5.png'),
+    textureLoader.load('/particles/6.png'),
+    textureLoader.load('/particles/7.png'),
+    textureLoader.load('/particles/8.png')
+]
 
-/**
- * Material
- */
-const materialParameters = {}
-materialParameters.color = '#70c1ff'
-gui.addColor(materialParameters, 'color').onChange(() =>
+const createFirework = (count, position, size, texture, radius, color) =>
 {
-    material.uniforms.uColor.value.set(materialParameters.color)
-})
+    const positionArray = new Float32Array(count * 3)
+    const sizesArray = new Float32Array(count)
+    const timeMultipliersArray = new Float32Array(count)
 
-const material = new THREE.ShaderMaterial(
+    for(let i = 0; i < count; i++)
     {
-        vertexShader: holographicVertexShader,
-        fragmentShader: holographicFragmentShader,
-        uniforms:
-        {
-            uTime: new THREE.Uniform(0),
-            uColor: new THREE.Uniform(new THREE.Color(materialParameters.color))
-        },
-        transparent: true,
-        side: THREE.DoubleSide,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending
+        const i3 = i * 3
+
+        const spherical = new THREE.Spherical(radius * (0.75 + Math.random() * 0.25),
+             Math.random() * Math.PI, Math.random() * Math.PI * 2)
+        const position = new THREE.Vector3()
+        position.setFromSpherical(spherical)
+
+        positionArray[i3] = position.x
+        positionArray[i3 + 1] = position.y
+        positionArray[i3 + 2] = position.z
+
+        sizesArray[i] = Math.random()
+        timeMultipliersArray[i] = 1 + Math.random()
     }
-)
+    const geometry = new THREE.BufferGeometry()
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionArray, 3))
+    geometry.setAttribute('aSize', new THREE.Float32BufferAttribute(positionArray, 1))
+    geometry.setAttribute('aTimeMultiplier', new THREE.Float32BufferAttribute(timeMultipliersArray, 1))
+
+    texture.flipY = false
+    const material = new THREE.ShaderMaterial(
+        {
+            vertexShader: fireworkVertexShader,
+            fragmentShader: fireworkFragmentShader,
+            uniforms:
+            {
+                uSize: new THREE.Uniform(size),
+                uResolution: new THREE.Uniform(sizes.resolution),
+                uTexture: new THREE.Uniform(texture),
+                uColor: new THREE.Uniform(color),
+                uProgress: new THREE.Uniform(0)
+            },
+            transparent: true,
+            depthWrite: false,
+            blending: THREE.AdditiveBlending
+        }
+    )
+    const firework = new THREE.Points(geometry, material)
+    firework.position.copy(position)
+    scene.add(firework)
+
+    const destroy = () =>
+    {
+        scene.remove(firework)
+        geometry.dispose()
+        material.dispose()
+    }
+
+    gsap.to(material.uniforms.uProgress, { value: 1, duration: 3, ease: 'linear', onComplete: destroy })
+}
+
+
+const createRandomFirework = () =>
+{
+    const count = Math.round(400 + Math.random() * 1000)
+    const position = new THREE.Vector3(
+        (Math.random() - 0.5) * 2,
+        Math.random(),
+        (Math.random() - 0.5) * 2
+    )
+    const size = 0.1 + Math.random() * 0.1
+    const texture = textures[Math.floor(Math.random() * textures.length)]
+    const radius = 0.5 + Math.random()
+    const color = new THREE.Color()
+    color.setHSL(Math.random(), 1, 0.7)
+    createFirework(count, position, size, texture, radius, color)
+}
+createRandomFirework()
+window.addEventListener('click', createRandomFirework)
 
 /**
- * Objects
+ * Sky
  */
-// Torus knot
-const torusKnot = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
-    material
-)
-torusKnot.position.x = 3
-scene.add(torusKnot)
+const sky = new Sky()
+sky.scale.setScalar(450000)
+scene.add(sky)
 
-// Sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(),
-    material
-)
-sphere.position.x = - 3
-scene.add(sphere)
+const sun = new THREE.Vector3()
 
-// Suzanne
-let suzanne = null
-gltfLoader.load(
-    './suzanne.glb',
-    (gltf) =>
-    {
-        suzanne = gltf.scene
-        suzanne.traverse((child) =>
-        {
-            if(child.isMesh)
-                child.material = material
-        })
-        scene.add(suzanne)
-    }
-)
+const skyParameters = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.95,
+    elevation: -2.2,
+    azimuth: 180,
+    exposure: renderer.toneMappingExposure
+}
+
+const updateSky = () =>
+{
+    const uniforms = sky.material.uniforms
+    uniforms['turbidity'].value = skyParameters.turbidity
+    uniforms['rayleigh'].value = skyParameters.rayleigh
+    uniforms['mieCoefficient'].value = skyParameters.mieCoefficient
+    uniforms['mieDirectionalG'].value = skyParameters.mieDirectionalG
+
+    const phi = THREE.MathUtils.degToRad(90 - skyParameters.elevation)
+    const theta = THREE.MathUtils.degToRad(skyParameters.azimuth)
+
+    sun.setFromSphericalCoords(1, phi, theta)
+
+    uniforms['sunPosition'].value.copy(sun)
+
+    renderer.toneMappingExposure = skyParameters.exposure
+    renderer.render(scene, camera)
+}
+
+gui.add(skyParameters, 'turbidity', 0.0, 20.0, 0.1).onChange(updateSky)
+gui.add(skyParameters, 'rayleigh', 0.0, 4, 0.001).onChange(updateSky)
+gui.add(skyParameters, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(updateSky)
+gui.add(skyParameters, 'mieDirectionalG', 0.0, 1, 0.001).onChange(updateSky)
+gui.add(skyParameters, 'elevation', -3, 10, 0.01).onChange(updateSky)
+gui.add(skyParameters, 'azimuth', - 180, 180, 0.1).onChange(updateSky)
+gui.add(skyParameters, 'exposure', 0, 1, 0.0001).onChange(updateSky)
+
+updateSky()
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-    material.uniforms.uTime.value = elapsedTime
-
-    // Rotate objects
-    if(suzanne)
-    {
-        suzanne.rotation.x = - elapsedTime * 0.1
-        suzanne.rotation.y = elapsedTime * 0.2
-    }
-
-    sphere.rotation.x = - elapsedTime * 0.1
-    sphere.rotation.y = elapsedTime * 0.2
-
-    torusKnot.rotation.x = - elapsedTime * 0.1
-    torusKnot.rotation.y = elapsedTime * 0.2
-
     // Update controls
     controls.update()
 
